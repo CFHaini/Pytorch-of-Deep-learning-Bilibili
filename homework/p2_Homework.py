5d1fae2440e35565ab61ca582074ad880f49b02e
He sy <2232206329@qq.com>
2025-01-07 21:42:05 +0800

    新增rnn重难点内容讲解jupyter

--- homework/p2_Homework.py
+++ homework/p2_Homework.py
@@ -26,6 +26,10 @@ def loss(y_hat: numpy.ndarray, y: float) -> numpy.ndarray:
 w_cor = numpy.arange(0.0, 4.0, 0.1)  # 权重w的取值范围
 b_cor = numpy.arange(-2.0, 2.1, 0.1)  # 偏置b的取值范围
 
+# 输出穷举的权重和偏置的数量
+print(f'穷举的权重数量: {len(w_cor)}')  # 打印权重的数量
+print(f'穷举的偏置数量: {len(b_cor)}')  # 打印偏置的数量
+
 # 此处直接使用矩阵进行计算
 w, b = numpy.meshgrid(w_cor, b_cor)  # 创建权重和偏置的网格
 mse = numpy.zeros(w.shape)  # 初始化均方误差矩阵
@@ -33,8 +37,10 @@ mse = numpy.zeros(w.shape)  # 初始化均方误差矩阵
 # 计算每对权重和偏置下的均方误差
 for x, y in zip(x_data, y_data):
     _y = forward(w, b, x)  # 计算预测值
+    print(len(_y))
     mse += loss(_y, y)  # 累加损失
 mse /= len(x_data)  # 计算平均损失
+print()
 
 # h = plt.contourf(w, b, mse)  # 可选：绘制等高线图
 
@@ -51,5 +57,5 @@ plt.show()  # 显示图形
 '''
 得到的三维图形展示了不同权重w和偏置b组合下的均方误差（MSE）。
 图中的每一个点代表一个(w, b)组合对应的MSE值，颜色的深浅表示误差的大小。
-通过观察图形，我们可以找到使得MSE最小的w和b值，从而优化我们的线性模型。
+通过观察图形，我们可以找到使得MSE最小的w和b值，图中最小点不是一个点，是很多最优点组成的线。
 '''
